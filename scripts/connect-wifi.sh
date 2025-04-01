dc93c59c1ad8525ee29f273823c8071efc5729b8
Subrath <subrathitachi6@gmail.com>
2025-04-01 10:31:12 +0530

    update

---
+++ scripts/connect-wifi.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+iDIR="$HOME/.config/mako/icons/brightness-mid.png"
+
+get_icon() {
+  echo "$iDIR"
+}
+
+iwctl station wlan0 scan
+iwctl station wlan0 connect AndroidAP_6771
+notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$(get_icon)" "Wifi Connected"
